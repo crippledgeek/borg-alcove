546d143951ee8ed081235583ab6b19ae31c58333
Mattias Carlsson <mattias.carlsson01@gmail.com>
2023-08-21 00:24:15 +0200

    Extract static data to supplier

---
+++ src/main/java/se/disabledsecurity/borg/alcove/functions/Functions.java
@@ -0,0 +1,20 @@
+package se.disabledsecurity.borg.alcove.functions;
+
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import io.vavr.Function0;
+
+public class Functions {
+
+	private Functions() {}
+	public static final Function0<CsvSchema> locationsSchema = Function0.of(() -> CsvSchema
+			.builder()
+			.addColumn("county")
+			.addColumn("municipality")
+			.addColumn("code")
+			.build()
+			.withSkipFirstDataRow(true)
+			.withColumnSeparator(';')).memoized();
+
+	public static final Function0<CsvMapper> csvMapper = Function0.of(CsvMapper::new).memoized();
+}
