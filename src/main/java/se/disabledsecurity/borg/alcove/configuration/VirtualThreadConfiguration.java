4128850233f03e17bc693fa6ed2dede80a69b795
Mattias Carlsson <mattias.carlsson01@gmail.com>
2023-09-22 01:48:39 +0200

    feat(app): add support for virtual threads

--- src/main/java/se/disabledsecurity/borg/alcove/configuration/VirtualThreadConfiguration.java
+++ src/main/java/se/disabledsecurity/borg/alcove/configuration/VirtualThreadConfiguration.java
@@ -3,14 +3,22 @@ package se.disabledsecurity.borg.alcove.configuration;
 import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.concurrent.Executors;
 
+@EnableAsync
 @Configuration
 public class VirtualThreadConfiguration {
 	@Bean
 	public <T extends org.apache.coyote.ProtocolHandler> TomcatProtocolHandlerCustomizer<T> protocolHandlerVirtualThreadExecutorCustomizer() {
 		return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+	}
+	@Bean
+	public AsyncTaskExecutor applicationTaskExecutor() {
+		return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
 	}
 
 }
